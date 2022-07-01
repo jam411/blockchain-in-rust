31e611ae173883de6c9be3a8582e4b551e1a9c49
jam411 <r.j.shimizu@jaist.ac.jp>
2022-07-01 23:41:27 +0900

    update block.rs

--- src/block.rs
+++ src/block.rs
@@ -1,11 +1,43 @@
+use super::*;
 use std::fmt::{self, Debug, Formatter};
 
-pub struct Block {}
+pub struct Block {
+    pub index: u32,
+    pub timestamp: u128,
+    pub hash: BlockHash,
+    pub prev_block_hash: BlockHash,
+    pub nunce: u64,
+    pub payload: String,
+}
 
 impl Debug for Block {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "Block")
+        write!(
+            f,
+            "Block[{}]: {} at: {} with: {}",
+            &self.index,
+            &hex::encode(&self.hash),
+            &self.timestamp,
+            &self.payload,
+        )
     }
 }
 
-impl Block {}
+impl Block {
+    pub fn new(
+        index: u32,
+        timestamp: u128,
+        prev_block_hash: BlockHash,
+        nunce: u64,
+        payload: String,
+    ) -> Self {
+        Block {
+            index,
+            timestamp,
+            hash: vec![0; 32],
+            prev_block_hash,
+            nunce,
+            payload,
+        }
+    }
+}
