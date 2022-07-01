cb5f8b10bb97e5207cd862e35f527921ab8b4163
jam411 <r.j.shimizu@jaist.ac.jp>
2022-07-02 00:04:41 +0900

    create hashable.rs

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,8 @@ fn main() {
     let block = Block::new(0, 0, vec![0; 32], 0, "Genesis block".to_owned());
 
     println!("{:?}", &block);
+
+    let h = block.hash();
+
+    println!("{:?}", &h);
 }
